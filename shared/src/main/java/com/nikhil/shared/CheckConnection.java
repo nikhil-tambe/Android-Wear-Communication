777d92b5d991abbbe001d805279e25dd359f838b
Mojo Jojo <nikhilptambe@gmail.com>
2017-07-15 12:58:55 +0530

    first commit

---
+++ shared/src/main/java/com/nikhil/shared/CheckConnection.java
@@ -0,0 +1,104 @@
+package com.nikhil.shared;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.os.Build;
+
+/**
+ * Created by Nikhil on 15/7/17.
+ */
+
+public class CheckConnection {
+
+    BluetoothAdapter bluetoothAdapter;
+    int denied;
+    private Activity activity;
+
+    public CheckConnection(Activity activity) {
+        this.activity = activity;
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        denied = PackageManager.PERMISSION_DENIED;
+    }
+
+    public boolean isBluetoothAvailable() {
+
+        return (bluetoothAdapter != null && bluetoothAdapter.isEnabled());
+    }
+
+    public void enableBluetooth() {
+        if (!bluetoothAdapter.isEnabled()) {
+            bluetoothAdapter.enable();
+        }
+    }
+
+    public boolean isInternetAvailable() {
+        ConnectivityManager connectivity = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null) {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if (info != null)
+                for (int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+        }
+        return false;
+    }
+
+    public boolean isInternetAvailable(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Network[] networks = connectivityManager.getAllNetworks();
+            NetworkInfo networkInfo;
+            for (Network mNetwork : networks) {
+                networkInfo = connectivityManager.getNetworkInfo(mNetwork);
+                if (networkInfo.getState().equals(NetworkInfo.State.CONNECTED)) {
+                    return true;
+                }
+            }
+        } else {
+            if (connectivityManager != null) {
+                //noinspection deprecation
+                NetworkInfo[] info = connectivityManager.getAllNetworkInfo();
+                if (info != null) {
+                    for (NetworkInfo anInfo : info) {
+                        if (anInfo.getState() == NetworkInfo.State.CONNECTED) {
+                            //Log.d(TAG, "Network: " + anInfo.getTypeName());
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean checkBTConnection() {
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        return mBluetoothAdapter.isEnabled();
+    }
+
+    /*
+    public boolean checkLocationPermission() {
+        int l1 = ContextCompat.checkSelfPermission(activity,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        int l2 = ContextCompat.checkSelfPermission(activity,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        return !(l1 == denied || l2 == denied);
+    }
+
+    public boolean checkStoragePermission() {
+        int s1 = ContextCompat.checkSelfPermission(activity,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        //ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        return s1 != denied;
+    }*/
+
+}
---
+++ shared/src/main/java/com/nikhil/shared/Constants.java
@@ -0,0 +1,30 @@
+package com.nikhil.shared;
+
+/**
+ * Created by Nikhil on 15/7/17.
+ */
+
+public interface Constants {
+
+
+    interface ChannelC{
+        String MESSAGE_CHANNEL = "/mess";
+    }
+
+    interface SPF {
+        String SPF_APP = "spf_app";
+        String KEY_DEVICE_NAME = "device_";
+        String KEY_DEVICE_MODEL = "device_";
+        String KEY_DEVICE_VERSION = "device_";
+        String KEY_DEVICE_MANUFACTURER = "device_";
+    }
+
+    interface StorageC {
+
+    }
+
+    interface IntentC{
+        //String KEY_
+    }
+
+}
